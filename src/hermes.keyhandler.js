42ab55bd945f27a215a757935cf290f5ba6e0c80
Jeremy Kahn <jeremyckahn@gmail.com>
2011-10-02 20:17:37 -0700

    Added some keyhandler glue code.

---
+++ src/hermes.keyhandler.js
@@ -0,0 +1,19 @@
+/**
+ * Requires:
+ *  hermes.core.js
+ */
+;(function (global) {
+  
+  var gh;
+  
+  gh = global.Hermes;
+  
+  gh.prototype.keyhandlerAdd = function keyhandlerAdd (key, handler) {
+    
+  };
+  
+  gh.prototype.keyhandlerRemove = function keyhandlerRemove (key, handler) {
+    
+  };
+  
+} (this));
