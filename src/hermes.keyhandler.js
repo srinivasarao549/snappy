b003270ea0414a2686f7bb8f33df5d29c7d9066d
Jeremy Kahn <jeremyckahn@gmail.com>
2011-10-02 20:49:07 -0700

    Basic key handling.

--- src/hermes.keyhandler.js
+++ src/hermes.keyhandler.js
@@ -4,12 +4,28 @@
  */
 ;(function (global) {
   
-  var gh;
+  var gh
+      ,keyMap;
   
   gh = global.Hermes;
   
+  gh.prototype.keys = {
+    'UP': 38
+    ,'DOWN': 40
+    ,'LEFT': 37
+    ,'RIGHT': 39
+  };
+  
   gh.prototype.keyhandlerAdd = function keyhandlerAdd (key, handler) {
     
+    eventHandler = function (ev) {
+      if (ev.keyCode === key) {
+        (handler || gh.util.noop)();
+      }
+    };
+    
+    eventHandler.name = 'eventHandler';
+    document.body.addEventListener('keydown', eventHandler, false);
   };
   
   gh.prototype.keyhandlerRemove = function keyhandlerRemove (key, handler) {
