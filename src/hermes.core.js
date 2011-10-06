892138a2a217dfdb5d84b38745875edab709f24d
Jeremy Kahn <jeremyckahn@gmail.com>
2011-10-05 20:49:13 -0700

    Cleaned up the whitespace.

--- src/hermes.core.js
+++ src/hermes.core.js
@@ -1,65 +1,65 @@
 /**
  * Requires:
- *  
+ *
  */
 ;(function (global) {
-  
+
   var gh;
-  
+
   /**
-	 * Handy shortcut for doing a for-in loop.  Takes care of all of the `hasOwnProperty` wizardry for you.
-	 * @param {Object} obj The object to iterate through.
-	 * @param {Function} func The function to pass the object and "own" property to.  This handler function receives the `obj` back as the first parameter, and a property name as the second.
-	 */
-	function each (obj, func) {
-		var prop;
-		
-		for (prop in obj) {
-			if (obj.hasOwnProperty(prop)) {
-				func(obj, prop);
-			}
-		}
-	}
-	
-	/**
-	 * Does a basic copy of one Object's properties to another.  This is not a robust `extend` function, nor is it recusrsive.  It is only appropriate to use on objects that have primitive properties (Numbers, Strings, Boolean, etc.).
-	 * @param {Object} targetObject The object to copy into
-	 * @param {Object} srcObject The object to copy from
-	 * @returns {Object} A reference to the augmented `targetObj` Object
-	 */
-	function simpleCopy (targetObj, srcObj) {
-		each(srcObj, function (srcObj, prop) {
-			targetObj[prop] = srcObj[prop];
-		});
-		
-		return targetObj;
-	}
-	
-	function noop () {
-	  // Does nothing.
-	}
-  
+   * Handy shortcut for doing a for-in loop.  Takes care of all of the `hasOwnProperty` wizardry for you.
+   * @param {Object} obj The object to iterate through.
+   * @param {Function} func The function to pass the object and "own" property to.  This handler function receives the `obj` back as the first parameter, and a property name as the second.
+   */
+  function each (obj, func) {
+    var prop;
+
+    for (prop in obj) {
+      if (obj.hasOwnProperty(prop)) {
+        func(obj, prop);
+      }
+    }
+  }
+
+  /**
+  * Does a basic copy of one Object's properties to another.  This is not a robust `extend` function, nor is it recusrsive.  It is only appropriate to use on objects that have primitive properties (Numbers, Strings, Boolean, etc.).
+  * @param {Object} targetObject The object to copy into
+  * @param {Object} srcObject The object to copy from
+  * @returns {Object} A reference to the augmented `targetObj` Object
+  */
+  function simpleCopy (targetObj, srcObj) {
+    each(srcObj, function (srcObj, prop) {
+      targetObj[prop] = srcObj[prop];
+    });
+
+    return targetObj;
+  }
+
+  function noop () {
+   // Does nothing.
+  }
+
   /**
    * Constructor.
    * @param {HTMLCanvasElement} canvas
    */
   gh = global.Hermes = global.Hermes || function Hermes (canvas) {
-    
+
     var self;
-    
+
     self = this;
     this.canvas = canvas;
     this.context = canvas.getContext('2d');
-    
+
     return this;
   };
-  
+
   gh.util = {};
-  
+
   simpleCopy(gh.util, {
     'noop': noop
     ,'simpleCopy': simpleCopy
     ,'each': each
   });
-  
+
 } (this));
--- src/hermes.keyhandler.js
+++ src/hermes.keyhandler.js
@@ -3,84 +3,84 @@
  *  hermes.core.js
  */
 ;(function (global) {
-  
+
   var gh
       ,keyMap;
-      
+
   function addIfUnique (arr, val) {
     var hasValue
         ,i;
-    
+
     hasValue = false;
-    
+
     for (i = 0; i < arr.length; i++) {
       if (arr[i] === val) {
         hasValue = true;
       }
     }
-    
+
     if (hasValue) {
       arr.push(value);
       return true;
     }
-    
+
     return false;
   }
-  
+
   function removeFirstInstanceOf (arr, val) {
     var i;
-    
+
     for (i = 0; i < arr.length; i++) {
       if (arr[i] === val) {
         arr.splice(i, 1);
         break;
       }
     }
-    
+
     return;
   }
-  
+
   gh = global.Hermes;
-  
+
   gh.prototype.keys = {
     'UP': 38
     ,'DOWN': 40
     ,'LEFT': 37
     ,'RIGHT': 39
   };
-  
+
   gh.prototype.keyhandlerInit = function keyhandlerInit () {
     var self;
-    
+
     self = this;
     this.keysDown = [];
-    
+
     function keydownHandler (ev) {
-      
+
     };
-    
+
     function keyupHandler (ev) {
-      
+
     };
-    
+
     document.body.addEventListener('keydown', keydownHandler, false);
     document.body.addEventListener('keyup', keyupHandler, false);
   }
-  
+
   gh.prototype.keyhandlerAdd = function keyhandlerAdd (key, handler) {
-    
+
     function eventHandler (ev) {
       if (ev.keyCode === key) {
         (handler || gh.util.noop)();
       }
     };
-    
+
     eventHandler.name = 'eventHandler';
     document.body.addEventListener('keypressed', eventHandler, false);
   };
-  
+
   gh.prototype.keyhandlerRemove = function keyhandlerRemove (key, handler) {
-    
+
   };
-  
+
 } (this));
