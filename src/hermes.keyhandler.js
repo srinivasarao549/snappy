8c999649a53b6f7d7c30807af722d8e50f8eb2a3
Jeremy Kahn <jeremyckahn@gmail.com>
2011-10-05 21:45:04 -0700

    Resolved dependency issues.

--- src/hermes.keyhandler.js
+++ src/hermes.keyhandler.js
@@ -2,89 +2,54 @@
  * Requires:
  *  hermes.core.js
  */
+
 ;(function (global) {
+  define(['src/hermes.core', 'lib/underscore'], function (Hermes) {
+    var keyMap;
 
-  var gh
-      ,keyMap;
-
-  /*function addIfUnique (arr, val) {
-    var hasValue
-        ,i;
-
-    hasValue = false;
-
-    for (i = 0; i < arr.length; i++) {
-      if (arr[i] === val) {
-        hasValue = true;
-      }
-    }
-
-    if (hasValue) {
-      arr.push(value);
-      return true;
-    }
-
-    return false;
-  }
-
-  function removeFirstInstanceOf (arr, val) {
-    var i;
-
-    for (i = 0; i < arr.length; i++) {
-      if (arr[i] === val) {
-        arr.splice(i, 1);
-        break;
-      }
-    }
-
-    return;
-  }*/
-
-  gh = global.Hermes;
-
-  gh.prototype.keys = {
-    'UP': 38
-    ,'DOWN': 40
-    ,'LEFT': 37
-    ,'RIGHT': 39
-  };
-
-  gh.prototype.keyhandlerInit = function keyhandlerInit () {
-    var self;
-
-    self = this;
-    this.keysdown = {};
-
-    function keydownHandler (ev) {
-      //addIfUnique(self.keysdown, ev.which);
-      self.keysdown[ev.which] = true;
-
+    Hermes.prototype.keys = {
+      'UP': 38
+      ,'DOWN': 40
+      ,'LEFT': 37
+      ,'RIGHT': 39
     };
 
-    function keyupHandler (ev) {
-      //removeFirstInstanceOf(self.keysdown, ev.which);
-      delete self.keysdown[ev.which];
-      console.log(self.keysdown+'');
+    Hermes.prototype.keyhandlerInit = function keyhandlerInit () {
+      var self;
+
+      self = this;
+      this.keysdown = {};
+
+      function keydownHandler (ev) {
+        //addIfUnique(self.keysdown, ev.which);
+        self.keysdown[ev.which] = true;
+
+      };
+
+      function keyupHandler (ev) {
+        //removeFirstInstanceOf(self.keysdown, ev.which);
+        delete self.keysdown[ev.which];
+        console.log(self.keysdown+'');
+      };
+
+      document.body.addEventListener('keydown', keydownHandler, false);
+      document.body.addEventListener('keyup', keyupHandler, false);
+    }
+
+    Hermes.prototype.keyhandlerAdd = function keyhandlerAdd (key, handler) {
+
+      function eventHandler (ev) {
+        if (ev.keyCode === key) {
+          (handler || Hermes.util.noop)();
+        }
+      };
+
+      eventHandler.name = 'eventHandler';
+      document.body.addEventListener('keypressed', eventHandler, false);
     };
 
-    document.body.addEventListener('keydown', keydownHandler, false);
-    document.body.addEventListener('keyup', keyupHandler, false);
-  }
+    Hermes.prototype.keyhandlerRemove = function keyhandlerRemove (key, handler) {
 
-  gh.prototype.keyhandlerAdd = function keyhandlerAdd (key, handler) {
-
-    function eventHandler (ev) {
-      if (ev.keyCode === key) {
-        (handler || gh.util.noop)();
-      }
     };
-
-    eventHandler.name = 'eventHandler';
-    document.body.addEventListener('keypressed', eventHandler, false);
-  };
-
-  gh.prototype.keyhandlerRemove = function keyhandlerRemove (key, handler) {
-
-  };
-
+  });
 } (this));
